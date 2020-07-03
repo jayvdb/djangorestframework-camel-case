b90e60985ec2220f8cd434cc1d0e97a2a622a889
John Vandenberg <jayvdb@gmail.com>
2020-07-03 17:27:24 +0700

    Add JSONResponse handler

--- djangorestframework_camel_case/middlewares.py
+++ djangorestframework_camel_case/middlewares.py
@@ -1,5 +1,11 @@
+import json
+
+from django.http import JsonResponse
+
+from rest_framework.response import Response
+
 from djangorestframework_camel_case.settings import api_settings
-from djangorestframework_camel_case.util import underscoreize
+from djangorestframework_camel_case.util import camelize, underscoreize
 
 
 class CamelCaseMiddleWare:
@@ -14,4 +20,18 @@ class CamelCaseMiddleWare:
         )
 
         response = self.get_response(request)
+
+        if isinstance(response, JsonResponse):
+            content = response._container[0]
+            data = json.loads(content)
+            content = json.dumps(camelize(
+                data,
+                **api_settings.JSON_UNDERSCOREIZE
+            )).encode('utf-8')
+            response._container = [content]
+
+            # It would be nice to build a
+            # JsonResponse(data=data, status=response.status_code)
+            # and then response.render()
+
         return response
